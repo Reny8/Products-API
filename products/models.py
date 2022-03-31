d24c973e100f058b4f5f76eadd47991dd4f6aca1
Reny Reyes <renyreyes8@hotmail.com>
2022-03-31 12:02:31 -0400

    Created my product model class

--- products/models.py
+++ products/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+# for string use CharField()
+# for number use IntegerField()
+# for decimal use DecimalField()
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length = 100)
+    description = models.CharField(max_length = 100)
+    price = models.DecimalField(max_digits = 8,decimal_places = 2)
+    inventory_quantity = models.IntegerField()
+    
