64f071d3f68df394f5194527b8be14d2cff30a95
Reny Reyes <renyreyes8@hotmail.com>
2022-03-31 16:53:02 -0400

    Created the product_detail function and created a new path for the pk

--- products/views.py
+++ products/views.py
@@ -3,12 +3,12 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Product
 from .serializers import ProductSerializer
-from django.shortcuts import render
+
 
 @api_view(['GET','POST'])
 def products_list(request):
-    product = Product.objects.all()
     if request.method == 'GET':
+        product = Product.objects.all()
         serializer = ProductSerializer(product, many = True)
         return Response(serializer.data)
     elif request.method == 'POST':
@@ -18,3 +18,13 @@ def products_list(request):
             return Response(serializer.data, status.HTTP_201_CREATED)
         else: 
             return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def product_detail(request,pk):
+    # if it does not find a product with that primary key
+    try: 
+        product = Product.objects.get(pk = pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    except Product.DoesNotExist:
+        return Response(status = status.HTTP_404_NOT_FOUND)
