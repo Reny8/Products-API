05ca9122f7fca99c384ad73e3531a39aefdf87ab
Reny Reyes <renyreyes8@hotmail.com>
2022-03-31 15:23:59 -0400

    Completed the products_list function + Tested on Postman

--- products/views.py
+++ products/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
+from django.shortcuts import render
+
 @api_view(['GET'])
 def products_list(request):
-    # provide a value later for response
-    return Response('ok')
+    product = Product.objects.all()
+    serializer = ProductSerializer(product, many = True)
+    # This takes back all the products created and returns it to python as a dictionary 
+    # The response will turn it into a JSON
+    return Response(serializer.data)
