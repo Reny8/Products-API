6c459636dee8a255f9250929295cdfa234ffa468
Reny Reyes <renyreyes8@hotmail.com>
2022-04-01 17:53:37 -0400

    Added product_id to the reviews table to connect to the Product item

--- reviews/models.py
+++ reviews/models.py
@@ -6,7 +6,7 @@ from products.models import Product
 class Review(models.Model):
     subject = models.CharField(max_length = 50)
     product_id = models.IntegerField()
-    stars = models.DecimalField(max_digits = 1, decimal_places = 1)
+    stars = models.DecimalField(max_digits = 2, decimal_places = 1)
     body = models.CharField(max_length = 300)
 
 
