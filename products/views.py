0dab32a1f93eeb72b436e8bf1f45726032881426
Reny Reyes <renyreyes8@hotmail.com>
2022-04-01 12:11:01 -0400

    Added a patch request for the laptop item + Tested on Postman

--- products/views.py
+++ products/views.py
@@ -1,10 +1,11 @@
+import string
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Product
 from .serializers import ProductSerializer
 from django.shortcuts import get_object_or_404
-
+from django.db import models
 
 @api_view(['GET','POST'])
 def products_list(request):
@@ -20,7 +21,7 @@ def products_list(request):
         else: 
             return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET','PUT','DELETE'])
+@api_view(['GET','PUT','DELETE','PATCH'])
 def product_detail(request,pk):
     product = get_object_or_404(Product, pk = pk)
     if request.method == 'GET':
@@ -34,3 +35,10 @@ def product_detail(request,pk):
     elif request.method == 'DELETE':
         product.delete()
         return Response(status = status.HTTP_204_NO_CONTENT)
+    elif request.method == 'PATCH':
+        # added a photo to the laptop product
+        if product.pk == 2:
+            product.description += 'Link: https://cdn.mos.cms.futurecdn.net/7WbSVGBMTEaXqu4XNVkX8N-768-80.jpg'
+            return Response(product.description)
+        else:
+            return Response(status = status.HTTP_400_BAD_REQUEST)
